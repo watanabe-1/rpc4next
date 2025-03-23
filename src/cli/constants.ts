7c6bbfe8ea85df25445921f30ef2233924a2fb6e
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2025-03-23 16:00:15 +0900

    refactor: remove unused PATH_PATH_STRUCTURE_BASE constant from constants.ts

--- src/cli/constants.ts
+++ src/cli/constants.ts
@@ -6,8 +6,6 @@ export const NEWLINE = "\n";
 export const STATEMENT_TERMINATOR = ";";
 export const TYPE_SEPARATOR = ";";
 
-export const PATH_PATH_STRUCTURE_BASE =
-  "./scripts/generateRpcClient/pathStructureBase.ts";
 export const TYPE_END_POINT = "Endpoint";
 export const TYPE_KEY_QUERY = "QueryKey";
 export const TYPE_KEY_OPTIONAL_QUERY = "OptionalQueryKey";
