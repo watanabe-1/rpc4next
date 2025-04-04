552acc7d62fab0eef7985ffb2bc44492f2df6631
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2025-04-05 01:42:59 +0900

    feat: add debounceOnceRunningWithTrailing to support trailing execution after async run

--- src/cli/debounce.ts
+++ src/cli/debounce.ts
@@ -1,14 +1,40 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const debounce = <T extends (...args: any[]) => void>(
+export const debounceOnceRunningWithTrailing = <
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  T extends (...args: any[]) => Promise<void> | void,
+>(
   func: T,
   delay: number
 ) => {
-  let timer: ReturnType<typeof setTimeout>;
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  let isRunning = false;
+  let pendingArgs: Parameters<T> | null = null;
+
+  const execute = async (...args: Parameters<T>) => {
+    isRunning = true;
+    try {
+      await func(...args);
+    } finally {
+      isRunning = false;
+
+      if (pendingArgs) {
+        const nextArgs = pendingArgs;
+        pendingArgs = null;
+        execute(...nextArgs);
+      }
+    }
+  };
 
   return (...args: Parameters<T>) => {
-    clearTimeout(timer);
+    if (timer) clearTimeout(timer);
+
     timer = setTimeout(() => {
-      func(...args);
+      if (isRunning) {
+        pendingArgs = args ?? [];
+
+        return;
+      }
+
+      execute(...(args ?? []));
     }, delay);
   };
 };
