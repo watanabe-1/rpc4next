509abaf97af38d4a23532d044f65b37f2cdea261
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2025-04-27 10:51:03 +0900

    chore: update test coverage reporting settings (#119)

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "html", "json", "json-summary"],
+      reportOnFailure: true,
       reportsDirectory: "./coverage",
       include: ["src/**/*.ts"],
       exclude: [
