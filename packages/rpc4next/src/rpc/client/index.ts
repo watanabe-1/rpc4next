643cf8c6198528527f433375fdf2ae3aa230086e
watanabe-1 <96782802+watanabe-1@users.noreply.github.com>
2026-08-21 22:27:20 +0900

    refactor(client): move rpc helper to dedicated export

--- packages/rpc4next/src/rpc/client/index.ts
+++ packages/rpc4next/src/rpc/client/index.ts
@@ -1,3 +1,2 @@
 export { createRpcClient } from "./rpc-client";
-export { createRpcHelper } from "./rpc-helper";
 export type { ParamsKey, ProcedureQueryInput, QueryKey, RpcEndpoint } from "./types";
